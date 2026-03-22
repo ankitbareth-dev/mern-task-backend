0df97fa2e0e8ed4fa8cf753693392f72e77fd5bc
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-03-22 13:19:39 +0530

    feat: add catch async middleware to catch async errors

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhoast:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
   }),
 );
 
--- controllers/ai.controller.ts
+++ controllers/ai.controller.ts
@@ -1,21 +1,14 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { askAIService } from "../services/ai.service";
+import catchAsync from "../middlewares/catchAsync";
 
-export const askAI = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { prompt } = req.body;
+export const askAI = catchAsync(async (req: Request, res: Response) => {
+  const { prompt } = req.body;
 
-    const result = await askAIService(prompt);
+  const result = await askAIService(prompt);
 
-    res.status(200).json({
-      success: true,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: result,
+  });
+});
