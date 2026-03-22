0bc7206e92faa7f08f1944bba726f118931b118b
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-03-22 12:52:00 +0530

    feat: setup app

--- app.ts
+++ app.ts
@@ -1,5 +1,21 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running",
+  });
+});
 
 export default app;
